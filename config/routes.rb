c5a4034cb0d1137fb491e44bbaab4398a3c39917
EC2 Default User <ec2-user@ip-172-31-35-141.ap-northeast-1.compute.internal>
2022-09-09 06:59:50 +0000

    コントローラーはpublic まで作った

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
+  devise_for :customers,skip: [:passwords], controllres: {
+    registration: "public/registration",
+    sessions: 'public/sessions'
+  }
+  devise_for :admins, skip: [:registrations, :passwords], controllres: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
